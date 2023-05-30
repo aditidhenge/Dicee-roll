b62ff27ac23052b4fe9d2dd21b20e94320f9a507
AditiDhenge <103129934+aditidhenge@users.noreply.github.com>
2023-05-30 13:17:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ document.querySelector("button").addEventListener("click",function(){
     var randomImageSource = "images/" + randomDiceImage;
 
     var image1 = document.querySelectorAll("img")[0];
+    image1.classList.add("roll-animation");
+    setTimeout(function(){
+        image1.classList.remove("roll-animation");
+    },1000);
     image1.setAttribute("src",randomImageSource);
 
 
@@ -16,6 +20,10 @@ document.querySelector("button").addEventListener("click",function(){
 
     var randomImagesrc2 = "images/" + randomDiceImage2;
     var image2 = document.querySelectorAll("img")[1];
+    image2.classList.add("roll-animation");
+    setTimeout(function(){
+        image2.classList.remove("roll-animation");
+    },1000);
     image2.setAttribute("src",randomImagesrc2);
 
     if(random1>random2)
